98ac030751ad912cab788fea87f72080e6bdfded
Wafffel <kristoferski.kw@gmail.com>
2025-06-27 17:20:03 +0200

    fix fish config bugs

--- fish/config.fish
+++ fish/config.fish
@@ -6,18 +6,15 @@ set fish_greeting
 starship init fish | source
 
 function prompt_newline --on-event fish_postexec
-    if test "$__fish_last_command" != "clear"
+    set current_cmd (echo $argv[1] | string trim)
+    if test "$current_cmd" != "clear"
         echo
     end
 end
 
+
 if status is-interactive
     and not set -q TMUX
     set session_name "main"
-    if tmux has-session -t $session_name ^/dev/null
-        exec tmux attach -t $session_name
-    else
-        exec tmux new -s $session_name
-    end
+    exec tmux new -A -s $session_name
 end
-
