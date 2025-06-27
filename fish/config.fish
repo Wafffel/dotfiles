9fc104cbb8c1b13ad619495909c90f3d11d558d1
Wafffel <kristoferski.kw@gmail.com>
2025-06-27 16:25:57 +0200

    fix terminal not opening when no tmux session

--- fish/config.fish
+++ fish/config.fish
@@ -6,11 +6,18 @@ set fish_greeting
 starship init fish | source
 
 function prompt_newline --on-event fish_postexec
-	echo
+    if test "$__fish_last_command" != "clear"
+        echo
+    end
 end
 
 if status is-interactive
-and not set -q TMUX
-    exec tmux attach || tmux new
+    and not set -q TMUX
+    set session_name "main"
+    if tmux has-session -t $session_name ^/dev/null
+        exec tmux attach -t $session_name
+    else
+        exec tmux new -s $session_name
+    end
 end
 
