1419cfbded3100c142567af7f7c78ae854d8c452
Wafffel <kristoferski.kw@gmail.com>
2025-07-12 19:11:07 +0200

    fix script

--- hypr/scripts/show_keybinds.sh
+++ hypr/scripts/show_keybinds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mainMod=$(cat "$HOME/.config/hypr/hyprland.conf" | grep "mainMod = " | sed -E 's/\$mainMod = //')
+mainMod=$(cat "$HOME/.config/hypr/hyprland.conf" 2>/dev/null | grep "mainMod = " 2>/dev/null | sed -E 's/\$mainMod = //' 2>/dev/null)
 
-options=$(cat "$HOME/.config/hypr/hyprland.conf" | grep "bind = " | sed -E "s/bind =//; s/,//; s/ +//; s/,/ -/; s/,//; s/\\\$mainMod/$mainMod/")
+options=$(cat "$HOME/.config/hypr/hyprland.conf" 2>/dev/null | grep "bind = " 2>/dev/null | sed -E "s/bind =//; s/,//; s/ +//; s/,/ -/; s/,//; s/\\\$mainMod/$mainMod/" 2>/dev/null)
 
 chosen="$(echo -e "$options" | rofi -p "Keybinds:" -dmenu)"
