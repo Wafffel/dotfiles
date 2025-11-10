f77cd3bc37f57ee4711f8e55774968c4587c1772
Wafffel <kristoferski.kw@gmail.com>
2025-11-10 21:29:39 +0100

    fix conform in nvim

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -3,34 +3,19 @@ return {
 		"stevearc/conform.nvim",
 		keys = {
 			{
-				-- Manually format the current buffer
 				"<leader>f",
 				function()
 					require("conform").format({ async = true, lsp_fallback = true })
 				end,
-				mode = "n", -- Normal mode
-				desc = "[F]ormat Buffer",
-			},
-			{
-				-- Manually format the visual selection
-				"<leader>f",
-				function()
-					require("conform").format({
-						range = {
-							start = vim.fn.getpos("'<"),
-							["end"] = vim.fn.getpos("'>"),
-						},
-						async = true,
-						lsp_fallback = true,
-					})
-				end,
-				mode = "v", -- Visual mode
-				desc = "[F]ormat Selection",
+				mode = { "n", "v" }, -- Apply in Normal and Visual modes
+				desc = "[F]ormat Buffer/Selection",
 			},
 		},
 		opts = {
 			formatters_by_ft = {
 				lua = { "stylua" },
+				c = { "clang-format" },
+				typst = { "typstyle" },
 			},
 		},
 	},
