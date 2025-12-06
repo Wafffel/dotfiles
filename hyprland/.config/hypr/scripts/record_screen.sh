c6265ad8278acc1fc46e25c72ddcb945bee112fc
Wafffel <kristoferski.kw@gmail.com>
2025-12-06 17:41:00 +0100

    add bash formatter in nvim and change screen record notifications

--- hyprland/.config/hypr/scripts/record_screen.sh
+++ hyprland/.config/hypr/scripts/record_screen.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-if pgrep -x "wf-recorder" > /dev/null
-then
+DIRECTORY="$HOME/Videos/Recordings"
+FILENAME="recording_$(date +'%Y-%m-%d_%H-%M-%S').mp4"
+mkdir -p "$DIRECTORY"
+
+if pgrep -x "wf-recorder" >/dev/null; then
   pkill -SIGINT wf-recorder
-  notify-send "Screen recording stopped"
+  notify-send "Screen recording stopped" "Saving to $DIRECTORY/$FILENAME"
 else
   CORDINATES=$(slurp)
   if [ -n "$CORDINATES" ]; then
-    DIRECTORY="$HOME/Videos/Recordings"
-    FILENAME="recording_$(date +'%Y-%m-%d_%H-%M-%S').mp4"
-    mkdir -p "$DIRECTORY"
-
     wf-recorder -g "$CORDINATES" -f "$DIRECTORY/$FILENAME" &
-
-    notify-send "Screen recording started" "Saving to $DIRECTORY/$FILENAME"
   fi
 fi
--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,26 +1,27 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		keys = {
-			{
-				"<leader>f",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = { "n", "v" }, -- Apply in Normal and Visual modes
-				desc = "[F]ormat Buffer/Selection",
-			},
-		},
-		opts = {
-			formatters_by_ft = {
-				lua = { "stylua" },
-				c = { "clang-format" },
-				typst = { "typstyle" },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-		},
-	},
+  {
+    "stevearc/conform.nvim",
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = { "n", "v" }, -- Apply in Normal and Visual modes
+        desc = "[F]ormat Buffer/Selection",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        c = { "clang-format" },
+        typst = { "typstyle" },
+        bash = { "shfmt" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+  },
 }
