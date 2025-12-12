d535d0592a5ca0618d6be4b48409d372622d478d
Wafffel <kristoferski.kw@gmail.com>
2025-12-12 13:04:48 +0100

    remove auto format on save

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,27 +1,23 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    keys = {
-      {
-        "<leader>f",
-        function()
-          require("conform").format({ async = true, lsp_fallback = true })
-        end,
-        mode = { "n", "v" }, -- Apply in Normal and Visual modes
-        desc = "[F]ormat Buffer/Selection",
-      },
-    },
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        c = { "clang-format" },
-        typst = { "typstyle" },
-        bash = { "shfmt" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-    },
-  },
+	{
+		"stevearc/conform.nvim",
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = { "n", "v" }, -- Apply in Normal and Visual modes
+				desc = "[F]ormat Buffer/Selection",
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				c = { "clang-format" },
+				typst = { "typstyle" },
+				bash = { "shfmt" },
+			},
+		},
+	},
 }
